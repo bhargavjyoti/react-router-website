8c6088194f605a5b86baa03e2f04ac7329ed39a8
Bhargav Kumar <champion.maverick360@gmail.com>
2023-02-21 23:54:32 +0530

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,12 @@ import Error from './pages/Error'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Navbar />} errorElement={<Error />}>
+    <Route path='/' element={<Navbar />}>
       <Route index element={<Home />} />
       <Route path='about' element={<About />} />
       <Route path='plans' element={<Plans />}/>
       <Route path='contact' element={<Contact />} />
+      <Route path='*' element={<Error />} />
     </Route>
   )
 )
